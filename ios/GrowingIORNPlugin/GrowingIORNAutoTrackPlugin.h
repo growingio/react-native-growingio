0b9feeba2d87e393090e988ba13efe3577162468
nico <529725067@qq.com>
2018-08-27 19:54:50 +0800

    改变技术策略

---
+++ ios/GrowingIORNPlugin/GrowingIORNAutoTrackPlugin.h
@@ -0,0 +1,14 @@
+//
+//  GrowingIORNAutoTrackPlugin.h
+//  GrowingIORNPlugin
+//
+//  Created by apple on 2018/8/27.
+//  Copyright © 2018年 GrowingIO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface GrowingIORNAutoTrackPlugin : NSObject<RCTBridgeModule>
+
+@end
---
+++ ios/GrowingIORNPlugin/GrowingIORNAutoTrackPlugin.m
@@ -0,0 +1,85 @@
+//
+//  GrowingIORNAutoTrackPlugin.m
+//  GrowingIORNPlugin
+//
+//  Created by apple on 2018/8/27.
+//  Copyright © 2018年 GrowingIO. All rights reserved.
+//
+
+#import "GrowingIORNAutoTrackPlugin.h"
+
+#define GrowingReactNativeAutoTrack @"GrowingReactNativeAutoTrack"
+
+@implementation GrowingIORNAutoTrackPlugin
+
+RCT_EXPORT_MODULE(GrowingIOModule);
+
+RCT_EXPORT_METHOD(onPagePrepare:(NSString *)page)
+{
+    Class class = NSClassFromString(GrowingReactNativeAutoTrack);
+    BOOL responds = [class respondsToSelector:@selector(onPagePrepare:)];
+    
+    if (!class || !responds) {
+        return;
+    }
+    [self dispatchInMainThread: ^{
+        [class performSelector:@selector(onPagePrepare:) withObject:page];
+        
+    }];
+}
+
+RCT_EXPORT_METHOD(onPageShow:(NSString *)page)
+{
+    Class class = NSClassFromString(GrowingReactNativeAutoTrack);
+    BOOL responds = [class respondsToSelector:@selector(onPageShow:)];
+    
+    if (!class || !responds) {
+        return;
+    }
+    [self dispatchInMainThread: ^{
+        [class performSelector:@selector(onPageShow:) withObject:page];
+        
+    }];
+}
+
+RCT_EXPORT_METHOD(prepareView:(NSNumber *)tag isClickable:(BOOL)isClickable parameters:(NSDictionary *)parameters)
+{
+    Class class = NSClassFromString(GrowingReactNativeAutoTrack);
+    BOOL responds = [class respondsToSelector:@selector(prepareView:parameters:)];
+    
+    if (!class || !responds) {
+        return;
+    }
+    if (tag == nil) {
+        return;
+    }
+    NSDictionary *tagClickDict = @{@"tag":tag, @"isClickable":[NSNumber numberWithBool:isClickable]};
+    [self dispatchInMainThread: ^{
+        [class performSelector:@selector(prepareView:parameters:) withObject:tagClickDict withObject:parameters];
+    }];
+}
+
+RCT_EXPORT_METHOD(onClick:(NSNumber *)tag)
+{
+    Class class = NSClassFromString(GrowingReactNativeAutoTrack);
+    BOOL responds = [class respondsToSelector:@selector(onClick:)];
+    
+    if (!class || !responds) {
+        return;
+    }
+    [self dispatchInMainThread: ^{
+        [class performSelector:@selector(onClick:) withObject:tag];
+        
+    }];
+}
+
+- (void)dispatchInMainThread:(void (^)(void))block
+{
+    if ([NSThread isMainThread]) {
+        block();
+    } else {
+        dispatch_async(dispatch_get_main_queue(), block);
+    }
+}
+
+@end
